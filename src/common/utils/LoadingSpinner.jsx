6a820d9e8aa89ecb4e44a1f3d52486cc0e2513b3
열일 <isawthat77@naver.com>
2022-05-30 16:12:28 +0900

    경로 수정

--- src/common/utils/LoadingSpinner.jsx
+++ src/common/utils/LoadingSpinner.jsx
@@ -4,18 +4,18 @@ import './LoadingSpinner.css'
 function LoadingSpinner() {
     return (
         <>
-        <div className='back'></div>
-        <div className='loading'>
-            <div className="sk-chase">
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
-                <div className="sk-chase-dot"></div>
+            <div className='back'></div>
+            <div className='loading'>
+                <div className="sk-chase">
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                    <div className="sk-chase-dot"></div>
+                </div>
+                <p>Loading</p>
             </div>
-            <p>Loading</p>
-        </div>
         </>
     )
 }
