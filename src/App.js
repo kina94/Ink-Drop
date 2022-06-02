df4b3ea7af7889d1931164853e238d8b008fabce
열일 <isawthat77@naver.com>
2022-06-02 19:11:07 +0900

    모바일 스크롤 문제 해결

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import '@fortawesome/fontawesome-free/js/all.js'
 import MainContainer from './views/MainContainer';
 
 function App({ userRepository, authService, bookRepository}) {
+  // let vh = window.innerHeight*0.01;
+  // document.documentElement.style.setProperty('--vh', `${vh}px`)
+  // window.addEventListener('resize', ()=>{
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`)
+  // })
   return (
     <div className='app'>
       <BrowserRouter>
